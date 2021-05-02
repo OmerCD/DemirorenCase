fe1de783f45eb9ed7eb001e3695c793eb5175bfc
Ömer Can Danacıoğlu <omercan.danacioglu@ext.kocsistem.com.tr>
2021-05-03 01:12:24 +0300

    Identity Server request change

--- DemirorenCase.Infrastructure/Services/AuthenticationService.cs
+++ DemirorenCase.Infrastructure/Services/AuthenticationService.cs
@@ -1,6 +1,8 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -16,6 +18,7 @@ using IdentityModel.Client;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace DemirorenCase.Infrastructure.Services
 {
@@ -27,7 +30,9 @@ namespace DemirorenCase.Infrastructure.Services
         private readonly ILogger<AuthenticationService> _logger;
         private readonly IdentityServerOptions _identityServerOptions;
 
-        public AuthenticationService(IRepository<User> userRepository, IMapper mapper, IHttpClientFactory httpClientFactory, ILogger<AuthenticationService> logger, IOptions<IdentityServerOptions> options)
+        public AuthenticationService(IRepository<User> userRepository, IMapper mapper,
+            IHttpClientFactory httpClientFactory, ILogger<AuthenticationService> logger,
+            IOptions<IdentityServerOptions> options)
         {
             _userRepository = userRepository;
             _mapper = mapper;
@@ -54,30 +59,57 @@ namespace DemirorenCase.Infrastructure.Services
 
         public async Task<LoginResultDto> LoginUserAsync(LoginUserDto loginUserDto, CancellationToken token = default)
         {
-            var discoveryDocumentResponse = await _identityHttpClient.GetDiscoveryDocumentAsync(cancellationToken: token);
-            if (discoveryDocumentResponse.IsError)
+            _identityHttpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //setup login data
+            var formContent = new FormUrlEncodedContent(new[]
             {
-                _logger.LogError(discoveryDocumentResponse.Error);
-                throw new Exception(discoveryDocumentResponse.Error);
+                new KeyValuePair<string, string>("grant_type", OidcConstants.GrantTypes.Password),
+                new KeyValuePair<string, string>("client_id", _identityServerOptions.ClientId),
+                new KeyValuePair<string, string>("client_secret", _identityServerOptions.ClientSecret),
+                new KeyValuePair<string, string>("scope", _identityServerOptions.Scope),
+                new KeyValuePair<string, string>("username", loginUserDto.UserName),
+                new KeyValuePair<string, string>("password", loginUserDto.Password),
+            });
+
+            //send request
+            var responseMessage = await _identityHttpClient.PostAsync("connect/token", formContent, token);
+
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                //get access token from response body
+                var responseJson = await responseMessage.Content.ReadAsStringAsync(token);
+                return JsonConvert.DeserializeObject<LoginResultDto>(responseJson);
             }
 
-            var tokenResponse = await _identityHttpClient.RequestPasswordTokenAsync(new PasswordTokenRequest
-            {
-                Address = discoveryDocumentResponse.TokenEndpoint,
-                ClientId = _identityServerOptions.ClientId,
-                ClientSecret = _identityServerOptions.ClientSecret,
-                Scope = _identityServerOptions.Scope,
-                UserName = loginUserDto.UserName,
-                Password = loginUserDto.Password,
-                GrantType = OidcConstants.GrantTypes.Password
-            }, cancellationToken: token);
-            if (tokenResponse.IsError)
-            {
-                _logger.LogError(tokenResponse.Error);
-                throw new Exception(tokenResponse.Error);
-            }
+            _logger.LogError(responseMessage.ReasonPhrase);
+            throw new Exception(responseMessage.ReasonPhrase);
 
-            return JsonConvert.DeserializeObject<LoginResultDto>(tokenResponse.Json.ToString());
+            // var discoveryDocumentResponse =
+            //     await _identityHttpClient.GetDiscoveryDocumentAsync(cancellationToken: token);
+            // if (discoveryDocumentResponse.IsError)
+            // {
+            //     _logger.LogError(discoveryDocumentResponse.Error);
+            //     throw new Exception(discoveryDocumentResponse.Error);
+            // }
+            //
+            // var tokenResponse = await _identityHttpClient.RequestPasswordTokenAsync(new PasswordTokenRequest
+            // {
+            //     Address = discoveryDocumentResponse.TokenEndpoint,
+            //     ClientId = _identityServerOptions.ClientId,
+            //     ClientSecret = _identityServerOptions.ClientSecret,
+            //     Scope = _identityServerOptions.Scope,
+            //     UserName = loginUserDto.UserName,
+            //     Password = loginUserDto.Password,
+            //     GrantType = OidcConstants.GrantTypes.Password
+            // }, cancellationToken: token);
+            // if (tokenResponse.IsError)
+            // {
+            //     _logger.LogError(tokenResponse.Error);
+            //     throw new Exception(tokenResponse.Error);
+            // }
+            //
+            // return JsonConvert.DeserializeObject<LoginResultDto>(tokenResponse.Json.ToString());
         }
     }
 }
